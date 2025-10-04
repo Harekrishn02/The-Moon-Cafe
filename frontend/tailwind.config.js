ec007a855c651d456d8cf75face1b3ffe7a6b922
madhavinfo02 <madhavinfo02@gmail.com>
2025-10-04 12:28:20 +0530

    Initial commit - frontend and backend project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}", // scans all React components
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         georgia: ["Georgia", "serif"],
+//         nunito: ["Nunito", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
